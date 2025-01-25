83007edfe24b0bfce71ed7d62e727f2181cbf44c
Andi Agung Dwi Arya <73697540+devnolife@users.noreply.github.com>
2025-01-25 17:37:39 +0800

    Add role-based routing and token validation
    Add role and token validation to restrict access and handle automatic logout.
    
    * **src/app/layout.jsx**
      - Add `checkUserRoleAndToken` function to validate user role and token before rendering the layout.
      - Redirect to login page if token is invalid or user role is not allowed.
    
    * **src/hooks/useGraphql.js**
      - Remove user data from localStorage and redirect to login if token is invalid.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,8 @@
-
 import InitColorSchemeScript from '@mui/material/InitColorSchemeScript'
 
 import 'react-perfect-scrollbar/dist/css/styles.css'
 
 import { getSystemMode } from '@core/utils/serverHelpers'
-
 
 import '@/app/globals.css'
 
@@ -16,13 +14,35 @@ export const metadata = {
     'Vuexy - MUI Next.js Admin Dashboard Template - is the most developer friendly & highly customizable Admin Dashboard Template based on MUI v5.'
 }
 
+const checkUserRoleAndToken = () => {
+  const storedUser = localStorage.getItem('user')
+
+  if (storedUser) {
+    try {
+      const parsedUser = JSON.parse(storedUser)
+      const { role, token } = parsedUser
+
+      if (!token || !role) {
+        throw new Error('Invalid token or role')
+      }
+
+      // Add more role-based logic here if needed
+    } catch {
+      localStorage.removeItem('user')
+      window.location.href = '/login'
+    }
+  } else {
+    window.location.href = '/login'
+  }
+}
 
 const RootLayout = async props => {
   const { children } = props
 
-
   const systemMode = await getSystemMode()
   const direction = 'ltr'
+
+  checkUserRoleAndToken()
 
   return (
     <html id='__next' lang='en' dir={direction} suppressHydrationWarning>
